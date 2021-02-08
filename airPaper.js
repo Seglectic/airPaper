9e2b2183e7a2b9bc710728b8cc7d490af396e5c9
Khal <segger@Khalbook-Pro.local>
2021-02-08 13:22:55 -0500

    Patched imgdownload with a delay

--- airPaper.js
+++ airPaper.js
@@ -107,7 +107,7 @@ const imgDownload = (url, path, callback) => {
   request.head(url, (err, res, body) => {
       request(url)
       .pipe(fs.createWriteStream(path))
-      .on('close', ()=>{gunzip(path, path+'.png', ()=>{fs.unlinkSync(path), callback} )})
+      .on('close', ()=>{gunzip(path, path+'.png', ()=>{fs.unlinkSync(path), setTimeout(callback,2000)} )})
   })
 }
 
