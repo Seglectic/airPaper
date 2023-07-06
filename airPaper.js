63959483ac2f138de6c905c85ca6a0638ea30e14
Segger <saintdane@gmail.com>
2023-07-06 00:43:13 -0400

    Whitespace Cleanup & comment additions

--- airPaper.js
+++ airPaper.js
@@ -1,5 +1,3 @@
-
- 
 			// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 			// ┃                                                        ┃
 			// ┃                        airPaper.js                     ┃
@@ -15,7 +13,10 @@
 			const request     = require('request');           // Make http requests for thumbnail data
 			const gunzip      = require('gunzip-file');       // Extractor for gzip'd thumbnails
 			
-			/* ------------------------------------ Global Vars ----------------------------------- */
+// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+// │                                                    Global Vars                                                    │
+// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+
 			var Version        = "1.2.0";                     // Current Version
 			var updateInterval = 120000;                      // How often to check new orders (in ms)
 			var activeBase     = "Work Orders";       				// Uncomment for Production: "Work Orders"
@@ -25,7 +26,9 @@
 			var getURL         = 'http://108.189.198.147'     // Local external IP for airtable to request web data
 			var serveDir       = './imgServe'                 // Local directory to host images
 			
-			/* ---------------------------- Airtable & Paperless Config --------------------------- */
+// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+// │                                            Airtable & Paperless Config                                            │
+// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 			
 			// File to hold most recent known paperless ID
 			var latestWO = 80;     
@@ -51,13 +54,14 @@
 				return;
 			}
 			
-			
-			/* ---------------------------------- Express Server ---------------------------------- */
-			
+	// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+	// │                                                  Express Server                                                   │
+	// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 			// ╭───────────────────────────────────────────────────────────╮
 			// │  Begin Express server for hosting image files.            │
 			// │    (Required for thumbnails to be sent to to Airtable)    │
 			// │    Port httPort MUST be forwarded for Airtables' access.  │
+			// │    Upon failure, resulting image will be blank.           │
 			// ╰───────────────────────────────────────────────────────────╯
 			const express = require('express');
 			var app = express();
@@ -67,19 +71,17 @@
 			app.get('imgServe/:fileName', (req, res) => {
 				const fileName = req.params.fileName;
 				const filePath = path.join(__dirname, fileName);
-				
 				res.sendFile(filePath, (err) => {
 					if (err) {
 						console.error(`Error sending file: ${err}`);
 						res.status(err.status || 500).end();
-					} else {
-						// console.log(`File sent: ${fileName}`);
 					}
 				});
 			});
 			
-			
-			/* -------------------------------- Shorthand Functions ------------------------------- */
+	// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+	// │                                                Shorthand Functions                                                │
+	// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 			
 			// ╭────────────────────────────────────────────────╮
 			// │  Log text to terminal with timestamp prefixed  │
@@ -88,7 +90,6 @@
 				var tS = moment().format('Do MMM, h:mm:ss a');
 				console.log(`「${tS}」 ${text}` )
 			}
-			
 			
 			// ╭──────────────────────────────────────────────────╮
 			// │  Places request for JSON data from Paperless API │
@@ -114,8 +115,7 @@
 					})
 				}).on('error', (e) => {console.error(`HTTP Error: ${e.message}`);});
 			}
-			
-			
+					
 			// ╭─────────────────────────────╮
 			// │  imgDownload                │
 			// │  Downloads image at URL     │
@@ -131,16 +131,16 @@
 			}
 			
 			
-			
-			/* -------------------------------- airPaper Callbacks -------------------------------- */
+	// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+	// │                                                airPaper Callbacks                                                 │
+	// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 			
 				// ╭────────────────────────────────────────╮
 				// │   //SECTION getNewWOs()                │
 				// │                                        │
 				// │  Checks for existence of new orders    │
 				// │  and runs getWO() for each new found.  │
-				// ╰────────────────────────────────────────╯
-			
+				// ╰────────────────────────────────────────╯		
 				function getNewWOs(){
 				var options = {
 					"headers": { "Authorization": `API-Token ${paperlessKey}`},
@@ -172,8 +172,6 @@
 			//!SECTION
 			
 							 
-			
-			
 			 // ╭───────────────────────────────────────╮
 			 // │     //SECTION getWO()                 │
 			 // │                                       │
@@ -181,7 +179,6 @@
 			 // │  runs sendAirTableObject with         │
 			 // │  returned data                        │
 			 // ╰───────────────────────────────────────╯
-			
 			 function getWO(num){
 				var options = {
 					"headers": { "Authorization": `API-Token ${paperlessKey}`},
@@ -192,9 +189,7 @@
 			}
 			// !SECTION
 			
-			
-			
-			
+	
 			// ╭─────────────────────────────────────────╮
 			// │          //SECTION createWO()           │
 			// │                                         │
@@ -202,35 +197,33 @@
 			// │   to be sent to Airtable as a new item  │
 			// ╰─────────────────────────────────────────╯
 			function createWO(paperData){
-					paperData.order_items.forEach(orderItem => {   		// Iterate through all actual parts in the order
-					var WObject = {																		// Create the Work Order object we'll send to Airtable to make a record
-						"fields": {
-							"Paperless ID#":Number(orderItem.id),
-							"Part":orderItem.filename.split('.').slice(0, -1).join('.'),
-							"Status": "PO Received",
-							"Qty Ordered":Number(orderItem.quantity),
-							"Due Date": paperData.ships_on, 
-							"Client": paperData.customer.company ? paperData.customer.company.business_name : `${paperData.customer.first_name} ${paperData.customer.last_name}`, 
-							"Purchase Orders": paperData.payment_details.purchase_order_number,
-							"Notes": paperData.private_notes,
-							"Stock Status":'Not Yet Ordered',
-							"Tooling Status": 'Not Yet Ordered',
-							"Picture":[],
+					paperData.order_items.forEach(orderItem => {   		             													// Iterate through all actual parts in the order (Since orders can have multiple parts)
+					var WObject = {																		                              			  // Create the Work Order object we'll send to Airtable to make a record
+						"fields": {                                                                           // Create the fields object
+							"Paperless ID#":Number(orderItem.id),                                               // Add the Paperless ID# to the 'Paperless ID#' field
+							"Part":orderItem.filename.split('.').slice(0, -1).join('.'),                        // Use the filename as the part name (remove the file extension)
+							"Status": "PO Received",                                                            // Default to 'PO Received' for status
+							"Qty Ordered":Number(orderItem.quantity),                                           // Add the quantity to the 'Qty Ordered' field
+							"Due Date": paperData.ships_on, 																										// Use the 'ships_on' date as the due date
+																																																  // If the customer has a company, use it, otherwise use their name:
+							"Client": paperData.customer.company ? paperData.customer.company.business_name : `${paperData.customer.first_name} ${paperData.customer.last_name}`,  
+							"Purchase Orders": paperData.payment_details.purchase_order_number,                 // Add the PO number to the 'Purchase Orders' field
+							"Notes": paperData.private_notes, 																								  // Add private notes to the 'Notes' field
+							"Stock Status":'Not Yet Ordered',                                 									// Default to 'Not Yet Ordered' for stock status
+							"Tooling Status": 'Not Yet Ordered',                              									// Default to 'Not Yet Ordered' for tooling status
+							"Picture":[],                                                     									// Array for pictures
 						}
 					}
 			
-					let comp = orderItem.components[0];				// Grab the first 'Component' for the order (orders can be assemblies that get messy)
-					WObject.fields["Finish"]       = comp.finishes[0] ? comp.finishes[0] : "";
-					WObject.fields["Material"]     = comp.material.name ? comp.material.name : "";
-					WObject.fields["PP Part Link"] = `https://app.paperlessparts.com/parts/viewer/${comp.part_uuid}`;
+					let comp = orderItem.components[0];																												// Grab the first 'component' for the part (parts can be assemblies of multiple components)
+					WObject.fields["Finish"]       = comp.finishes[0] ? comp.finishes[0] : "";							 
+					WObject.fields["Material"]     = comp.material.name ? comp.material.name : ""; 					  
+					WObject.fields["PP Part Link"] = `https://app.paperlessparts.com/parts/viewer/${comp.part_uuid}`; 
 					WObject.fields["STEP File"]    = comp.part_url;
-					let thumbURL=comp.thumbnail_url;   																											  //Grab the Thumbnail URL from Paperless
-					// ╭───────────────────────────────────────────────────╮
-					// │  Send to Airtable if not export controlled (ITAR) │
-					// ╰───────────────────────────────────────────────────╯
-					let ITAR = orderItem.export_controlled // bool
+					let thumbURL=comp.thumbnail_url;   																											  // Grab the Thumbnail URL from Paperless
+					let ITAR = orderItem.export_controlled  																									// Send to Airtable if not export controlled (ITAR is a boolean)
 					if (!ITAR){																																							  // Grab the pic and send it if it's not ITAR-y
-						if (thumbURL){																																		    	// If the component in the order has an attached pic
+						if (thumbURL){																																		    	// If the part in the order has an attached pic
 							let fileName = WObject.fields["Part"].replace(/[ &\/\\#,+()$~%.'":*?<>{}]/g, '')      // Purge weird shit from the part name
 							let localThumbURL = `${getURL}:${httPort}/imgServe/${fileName}.png`;                  // Assemble the URL for where the image file is hosted locally
 							WObject.fields["Picture"].push({url:localThumbURL});                                  // Pushes an object to the Picture array with our local image address
@@ -239,18 +232,16 @@
 						}else{
 							sendAirtableObject(WObject);																												  // Send it without a pic if we don't have one
 						}
-					}else{ 																																			 							// If we've got an ITAR component
+					}else{ 																																			 							// If we've got an ITAR part
 						let ITARURL = `${getURL}:${httPort}/imgServe/ITAR.png`; 									 							// grab our local ITAR pic URL
 						WObject.fields["Picture"].push({url:ITARURL});                                    			// and attach the ITAR png to the object to send
 						sendAirtableObject(WObject);																							 							// then send the object per usual
 					}; // End ITAR check	
-			
 				}); //End for loop of each order
-			}
-			//!SECTION
+			}; //End createWO()
+			//!SECTION 
 			
-			
-			
+
 			 // ╭──────────────────────────────────────╮
 			 // │   //SECTION sendAirtableObject()     │
 			 // │                                      │
@@ -258,17 +249,16 @@
 			 // │  at 'activeBase'                     │
 			 // ╰──────────────────────────────────────╯
 			function sendAirtableObject(WObject){
-					base(activeBase).create([WObject], (err, records)=>{
-						if (err) {console.error(err);return;}
-						records.forEach(function (record) {
-							timePrint(`New entry: ${record.fields.Part} ( ${record.getId()} ) added.`);
-						});
-					});
+				base(activeBase).create([WObject], (err, records)=>{
+					if (err) {console.error(err);return;}
+					records.forEach(function (record) {timePrint(`New entry: ${record.fields.Part} ( ${record.getId()} ) added.`);});
+				});
 			}
 			//!SECTION
-			
-			/* -------------------------------------- Execute ------------------------------------- */
-			
+		
+	// ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+	// │                                                      Execute                                                      │
+	// └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 			timePrint(`AirPaper ${Version}`)																													// Print startup time
 			app.listen(httPort,function(){timePrint(`Server started on port ${httPort}.`);});         // Launch the webserver for hosting image files
 			getNewWOs();															  																							// Check now for any new Work ORders
